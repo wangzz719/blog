8f1724301d45136f8512357e696389f53bef0509
wangzhizhao <wangzhizhao@zhihu.com>
2018-04-09 23:56:11 +0800

    fix longestIncrSubArrayLen

--- algorithm/longestincrsubarray.go
+++ algorithm/longestincrsubarray.go
@@ -9,13 +9,16 @@ import (
 // func longestIncrSubArrayLen(nums []int) []int 获取以 nums[i] 元素
 // 结尾的最长上升子数列的长度
 // length[i] = max{length[k] + 1| num[k] < num[i] (0< k < i)}
-func longestIncrSubArrayLen(nums []int) []int {
+func longestIncrSubArrayLen(nums []int) int {
+	if len(nums) == 0{
+		return 0
+	}
 	length := make([]int, len(nums), len(nums))
 	length[0] = 1
 	for i := 1; i < len(nums); i++ {
-		maxLen := 0
+		maxLen := 1
 		for j := 0; j < i; j++ {
-			tmpLen := 0
+			tmpLen := 1
 			if nums[j] < nums[i] {
 				tmpLen = length[j] + 1
 			}
@@ -25,11 +28,11 @@ func longestIncrSubArrayLen(nums []int) []int {
 		}
 		length[i] = maxLen
 	}
-	return length
+	sort.Ints(length)
+	return length[len(length)-1]
 }
 
 func main() {
-	length := longestIncrSubArrayLen([]int{1, 7, 2, 8, 3, 4})
-	sort.Ints(length)
-	fmt.Println(length[len(length)-1])
+	length := longestIncrSubArrayLen([]int{10,9,2,5,3,7,101,18})
+	fmt.Println(length)
 }
